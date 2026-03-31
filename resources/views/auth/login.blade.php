02f05e7705d724944527973b67ff8cb0a3a992a6
Rafii Mafif <rafiimafif@gmail.com>
2026-04-01 00:28:35 +0700

    Remove purple sidebar from login page - single white card layout
    - Removed empty purple left column (col-lg-6 d-none d-lg-block bg-login-image)
    - Changed login form to full width inside card (col-lg-12)
    - Adjusted outer container to col-xl-6 for better proportions
    - Login page now displays as single centered white form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,15 +17,13 @@
         <!-- Outer Row -->
         <div class="row justify-content-center">
 
-            <div class="col-xl-10 col-lg-12 col-md-9">
+            <div class="col-xl-6 col-lg-8 col-md-10">
 
                 <div class="card o-hidden border-0 shadow-lg my-5">
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
-                            <div class="col-lg-6 d-none d-lg-block bg-login-image" style="background: #4e73df url('/img/automateCRM.png') center/contain no-repeat;">
-                            </div>
-                            <div class="col-lg-6">
+                            <div class="col-lg-12">
                                 <div class="p-5">
                                     <div class="text-center">
                                         <img src="/img/automateCRM.png" alt="automateCRM" class="mb-3" style="width:80px;height:80px;object-fit:contain;">
