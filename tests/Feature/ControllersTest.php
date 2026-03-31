15df6bae47d5be511dfe37e01a8f2417f272edb9
Rafii Mafif <rafiimafif@gmail.com>
2026-03-31 21:33:09 +0700

    style: remove unused imports in ControllersTest

--- tests/Feature/ControllersTest.php
+++ tests/Feature/ControllersTest.php
@@ -2,9 +2,6 @@
 
 namespace Tests\Feature;
 
-use App\Models\Customer;
-use App\Models\Service;
-use App\Models\ServicetoCustomer;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\DB;
